49687e8f3cd98ce2f817c62c5960370e21f706d6
Oleksandr Ignatenko <avi.cpp@gmail.com>
2020-12-18 17:08:01 +0100

    Setup CORS

--- project/__init__.py
+++ project/__init__.py
@@ -3,9 +3,11 @@ import os
 from flask import Flask
 from flask_admin import Admin
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+cors = CORS()
 admin = Admin(template_mode="bootstrap3")
 bcrypt = Bcrypt()
 
@@ -20,6 +22,7 @@ def create_app(script_info=None):
     app.config.from_object(app_settings)
 
     db.init_app(app)
+    cors.init_app(app, resources={r"*": {"origins": "*"}})
     bcrypt.init_app(app)
     if os.getenv("FLASK_ENV") == "development":
         admin.init_app(app)
